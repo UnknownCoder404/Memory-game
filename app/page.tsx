c9b0e3947cb23438e90a3cc19bc1b44410a04ec6
Jakov <qwerasdftzu@gmail.com>
2024-12-04 21:50:44 +0100

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import MemoGame from "./components/MemoGame";
+
+export const metadata: Metadata = {
+    title: "Memory igra",
+    description:
+        "Memory igra za Project Oxygen dizajnirana na temu prevencija pušenja",
+};
 
 export default function Home() {
     return <MemoGame />;
